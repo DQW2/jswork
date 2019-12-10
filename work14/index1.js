1a94f134d63ad0019961b3821525ef477e4ec5e5
DQW2 <2460496001@qq.com>
2019-12-10 13:13:47 +0800

    work14

--- work14/index1.js
+++ work14/index1.js
@@ -1,43 +1,43 @@
-var lamp={
+var lamp = {
     red: {
-        obj: document.getElementById('red'),
+        obj:document.getElementById('red'),
         timeout: 3,
         style: ['red','gray','gray'],
-        next: 'green'
+        next:'green'
     },
     yellow: {
-        obj: document.getElementById('yellow'),
+        obj:document.getElementById('yellow'),
         timeout: 2,
         style: ['gray','yellow','gray'],
-        next: 'red'    
+        next:'red'
     },
     green: {
-        obj: document.getElementById('green'),
+        obj:document.getElementById('green'),
         timeout: 5,
         style: ['gray','gray','green'],
-        next: 'yellow'
+        next:'yellow'
     },
-    changeStyle(style){
+    changeStyle(style) {
         this.red.obj.className = style[0];
         this.yellow.obj.className = style[1];
         this.green.obj.className = style[2];
     }
 };
-var count={
-    obj: document.getElementById('count'),
+var count = {
+    obj:document.getElementById('count'),
     change: function(num){
-        this.obj.innerHTML=(num<10)?('0'+num) : num;
+    this.obj.innerHTML = (num < 10) ? ('0' + num) : num ;
     }
 };
-var now=lamp.green;
+var now = lamp.green;
 var timeout = now.timeout;
 lamp.changeStyle(now.style);
 count.change(timeout);
-setInterval(function(){
-    if(--timeout<=0){
-        now=lamp[now.next];
+setInterval(function() {
+    if (--timeout <= 0) {
+        now = lamp[now.next];
         timeout = now.timeout;
         lamp.changeStyle(now.style);
     }
     count.change(timeout);
-},1000);   
+}, 1000);
